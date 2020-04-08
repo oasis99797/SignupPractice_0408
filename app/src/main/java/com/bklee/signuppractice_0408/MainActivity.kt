5168ddcd1ead9dbc813128c07e7e806e48224882
oasis99797 <oasis99797@gmail.com>
2020-04-08 21:10:13 +0900

    다음 버튼 누르면 마이페이지로 이동

--- app/src/main/java/com/bklee/signuppractice_0408/MainActivity.kt
+++ app/src/main/java/com/bklee/signuppractice_0408/MainActivity.kt
@@ -2,11 +2,20 @@ package com.bklee.signuppractice_0408
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nextBtn.setOnLongClickListener {
+
+            // MyPageActivity로 이동
+
+            val myIntent = Intent(this, MyPageActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
