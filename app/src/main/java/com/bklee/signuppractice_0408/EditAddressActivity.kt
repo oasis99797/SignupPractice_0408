4c6bd9a7eff606bc79e0c15f4cbedae916a7c32b
oasis99797 <oasis99797@gmail.com>
2020-04-08 23:42:17 +0900

    데이터 입력화면에서 가져오기(집에서 수정)

--- app/src/main/java/com/bklee/signuppractice_0408/EditAddressActivity.kt
+++ app/src/main/java/com/bklee/signuppractice_0408/EditAddressActivity.kt
@@ -1,7 +1,10 @@
 package com.bklee.signuppractice_0408
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_address.*
 
 class EditAddressActivity : AppCompatActivity() {
 
--- app/src/main/java/com/bklee/signuppractice_0408/EditNameActivity.kt
+++ app/src/main/java/com/bklee/signuppractice_0408/EditNameActivity.kt
@@ -1,5 +1,6 @@
 package com.bklee.signuppractice_0408
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
