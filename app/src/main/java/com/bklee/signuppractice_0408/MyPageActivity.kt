ea397538bc0b6e974a7ccfa4eea53480bae14b2b
oasis99797 <oasis99797@gmail.com>
2020-04-08 21:04:06 +0900

    액티비티 추가

---
+++ app/src/main/java/com/bklee/signuppractice_0408/MyPageActivity.kt
@@ -0,0 +1,12 @@
+package com.bklee.signuppractice_0408
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page)
+    }
+}
