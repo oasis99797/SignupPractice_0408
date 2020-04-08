9120020d685d486c2d8db5eeb3b31c129660cbfe
oasis99797 <oasis99797@gmail.com>
2020-04-08 22:27:06 +0900

    데이터 입력화면에서 가져오기

---
+++ app/src/main/java/com/bklee/signuppractice_0408/EditAddressActivity.kt
@@ -0,0 +1,29 @@
+package com.bklee.signuppractice_0408
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditAddressActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_address)
+
+        okBtn.setOnClickListener {
+            //            입력을 제대로 완료하겠다. => 돌아가는길에 데이터 첨부.
+//            데이터 첨부 전용 Intent 생성 => 출발지/목적지 적지 않는다.
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("mainAddress", mainAddressEdt.text.toString())
+            resultIntent.putExtra("detailAddress", detailAddressEdt.text.toString())
+
+//            입력 완료를 눌렀다 명시 + 들고갈 데이터 : resultIntent에 있다.
+            setResult(Activity.RESULT_OK, resultIntent)
+
+//            결과 세팅이 끝났으니 화면 종료.
+            finish()
+
+
+        }
+    }
+}
--- app/src/main/java/com/bklee/signuppractice_0408/EditNameActivity.kt
+++ app/src/main/java/com/bklee/signuppractice_0408/EditNameActivity.kt
@@ -7,9 +7,17 @@ import kotlinx.android.synthetic.main.activity_edit_name.*
 
 class EditNameActivity : AppCompatActivity() {
 
+    //    1000이라는 숫자가, 주소를 얻기위한 요청이라고 메모.
+    val REQ_FOR_ADDRESS = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_name)
+
+        inputAddressBtn.setOnClickListener {
+            val myIntent = Intent(this, EditAddressActivity::class.java)
+            startActivityForResult(myIntent, REQ_FOR_ADDRESS)
+        }
 
         okBtn.setOnClickListener {
 
@@ -24,4 +32,25 @@ class EditNameActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+//        요청 종류 확인
+        if (requestCode == REQ_FOR_ADDRESS) {
+//            확인을 눌러서 종료된게 맞는지
+            if (resultCode == Activity.RESULT_OK) {
+//                첨부된 데이터를 받자.
+
+                val main = data?.getStringExtra("mainAddress")
+                val detail = data?.getStringExtra("detailAddress")
+
+                addressTxt.text = "${main} / ${detail}"
+
+            }
+        }
+
+
+    }
+
 }
